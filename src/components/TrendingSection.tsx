d30c235c2933dcf8d0561ee9c605905694ae9dc0
Antonella Schillaci <antonella.schillaci145@gmail.com>
2025-07-16 17:43:39 +0200

    feat(ui): add FAQSection component and update TrendingSection

--- src/components/TrendingSection.tsx
+++ src/components/TrendingSection.tsx
@@ -1,3 +1,4 @@
+import FAQSection from "./FAQSection";
 import GradientCard from "./GradientCard";
 import MoreReasons from "./MoreReasons";
 import TopTitles from "./TopTitles";
@@ -11,6 +12,8 @@ const TrendingSection = () => {
       <TopTitles />
       <h3 className={styles.titleTop}>Motivi in più per abbonarsi</h3>
       <MoreReasons />
+      <h3 className={styles.titleTop}>Domande frequenti</h3>
+      <FAQSection />
     </section>
 
   );
