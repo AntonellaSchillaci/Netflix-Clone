17f504d417de886927f03efc4896418bcc8320d7
Antonella Schillaci <antonella.schillaci145@gmail.com>
2025-07-16 16:02:33 +0200

    feat(ui): add TopTitles component and update TrendingSection styles and logic

--- src/components/TrendingSection.tsx
+++ src/components/TrendingSection.tsx
@@ -1,12 +1,13 @@
 import GradientCard from "./GradientCard";
+import TopTitles from "./TopTitles";
 import styles from "./TrendingSection.module.scss";
 
 const TrendingSection = () => {
   return (
     <section className={styles.trendingSection}>
       <GradientCard />
-      
-      
+      <h3 className={styles.titleTop}>I titoli del momento</h3>
+      <TopTitles />
     </section>
 
   );
